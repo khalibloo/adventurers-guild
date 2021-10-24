03222e993f4694078cd736f30e3c81904d763284
Khalifa Lame <khalibloo@gmail.com>
2021-10-24 20:53:48 +0400

    fix: sentry init not being called

--- src/pages/_theme.tsx
+++ src/pages/_theme.tsx
@@ -1,9 +1,35 @@
 /* eslint-disable react/prop-types */
 import { Spin } from "antd";
 import type { Theme } from "vite-plugin-react-pages";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { configResponsive } from "ahooks";
 
-import NotFoundPage from "./404";
 import App from "@/App";
+import config from "@/utils/config";
+import NotFoundPage from "./404";
+
+if (config.sentryDSN) {
+  Sentry.init({
+    dsn: config.sentryDSN,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+// Responsive
+configResponsive({
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1600,
+});
 
 const CustomTheme: Theme = ({ loadedData, loadState }) => {
   // console.log("#Theme", staticData, loadedData, loadState);
