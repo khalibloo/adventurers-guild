1595cdde13f7f3f0d434aab837055f1b1247e752
Khalifa Lame <khalibloo@gmail.com>
2021-11-23 16:37:13 +0400

    feat: new ui design

--- src/pages/index$.tsx
+++ src/pages/index$.tsx
@@ -1,5 +1,11 @@
-import { Button, Col, Image, List, Row, Skeleton, Typography } from "antd";
-import { LikeOutlined, MessageOutlined, StarOutlined } from "@ant-design/icons";
+import { Button, Card, Col, Input, List, Row, Skeleton, Space, Typography } from "antd";
+import {
+  GlobalOutlined,
+  MoreOutlined,
+  SaveOutlined,
+  SearchOutlined,
+  ShareAltOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
 import BasicLayout from "@/layouts/BasicLayout";
@@ -9,53 +15,112 @@ const Home: React.FC = () => {
   const { data, isLoading } = useQuests();
 
   return (
-    <BasicLayout pageTitle="Quest Board" pageDescription="Find your next adventure">
-      <Typography.Title level={3} className="text-center">
-        Quest Board
-      </Typography.Title>
+    <BasicLayout pageTitle="Quest Board" pageDescription="Find your next adventure" noPadding>
+      <Row className="bg-primary-tint py-12" justify="center">
+        <Col span={16} xs={22} sm={22} md={18} lg={16}>
+          <Row gutter={[16, 16]} justify="space-between">
+            <Col span={10} xs={24} sm={24} md={10}>
+              <Input
+                size="large"
+                placeholder="Search jobs, keywords, req ID..."
+                suffix={<SearchOutlined style={{ color: "#bfbfbf" }} />}
+              />
+            </Col>
+            <Col span={10} xs={24} sm={24} md={10}>
+              <Input
+                size="large"
+                placeholder="Search location"
+                suffix={<GlobalOutlined style={{ color: "#bfbfbf" }} />}
+              />
+            </Col>
+            <Col span={4} xs={24} sm={24} md={4}>
+              <Button block size="large" type="primary">
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
       <Row justify="center">
         <Col span={16} xs={22} sm={22} md={18} lg={16}>
           {isLoading ? (
             <List
               itemLayout="vertical"
               dataSource={[1, 2, 3, 4, 5, 6]}
+              split={false}
               renderItem={(item) => <Skeleton active loading avatar key={item} />}
             />
           ) : (
             <List
               itemLayout="vertical"
               dataSource={data?.quests}
+              split={false}
               renderItem={(item) => (
-                <List.Item
-                  key={item.id}
-                  actions={[
-                    <Button type="link">
-                      <StarOutlined />
-                    </Button>,
-                    <Button type="link">
-                      <LikeOutlined />
-                    </Button>,
-                    <Button type="link">
-                      <MessageOutlined />
-                    </Button>,
-                  ]}
-                >
+                <List.Item key={item.id}>
                   <Link to={`/quests/${item.id}`} className="w-full">
-                    <List.Item.Meta
-                      avatar={
-                        <Image
-                          className="rounded-full"
-                          src={`https://avatars.dicebear.com/api/adventurer/${item.id}.svg`}
-                          alt="Quest giver"
-                          width="56"
-                          height="56"
-                          style={{ width: 56, height: 56 }}
-                          preview={false}
-                        />
-                      }
-                      title={item.title}
-                      description={item.description}
-                    />
+                    <Card hoverable size="small">
+                      <Row justify="space-between">
+                        <Col>
+                          <div>
+                            <Typography.Text className="text-primary" strong>
+                              {item.title}
+                            </Typography.Text>
+                          </div>
+                          <div>
+                            <Typography.Text type="secondary">{item.location}</Typography.Text>
+                          </div>
+                          <div>
+                            <Typography.Text>{item.salary}</Typography.Text>
+                          </div>
+                          <div>
+                            <Typography.Text>{item.type}</Typography.Text>
+                          </div>
+                        </Col>
+                        <Col>
+                          <Row>
+                            <Col xs={4} md={0}>
+                              <Button type="text" icon={<MoreOutlined />} />
+                            </Col>
+                            <Col span={24} xs={0} md={24}>
+                              <Space>
+                                <Button
+                                  className="text-primary"
+                                  type="text"
+                                  icon={<ShareAltOutlined />}
+                                >
+                                  Share
+                                </Button>
+                                <Button
+                                  className="text-primary"
+                                  type="text"
+                                  icon={<SaveOutlined />}
+                                >
+                                  Save
+                                </Button>
+                                <Button type="primary">Apply</Button>
+                              </Space>
+                            </Col>
+                          </Row>
+                        </Col>
+                      </Row>
+                      <Row className="mt-6" align="bottom" gutter={24}>
+                        <Col span={20} xs={0} md={16} lg={18}>
+                          <Typography.Paragraph
+                            className="mb-0"
+                            type="secondary"
+                            ellipsis={{ rows: 2 }}
+                          >
+                            {item.description} Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Rerum officia mollitia cumque recusandae libero ea totam qui,
+                            voluptates dolores error, ratione id et nam praesentium amet, impedit
+                            tempora similique omnis.
+                          </Typography.Paragraph>
+                        </Col>
+                        <Col span={4} xs={24} md={8} lg={6} className="text-right">
+                          <Typography.Text type="secondary">Posted 5 days ago</Typography.Text>
+                        </Col>
+                      </Row>
+                    </Card>
                   </Link>
                 </List.Item>
               )}
